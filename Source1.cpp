ee06f6617304af195a2b4eee5a3df467101aa7f2
BenJoice <71026189+BenJoice@users.noreply.github.com>
2022-02-08 22:28:04 +0300

    Add files via upload

---
+++ Source1.cpp
@@ -0,0 +1,13 @@
+#include"Header.h"
+//#include<iostream>
+//int main() {
+//	String a;
+//	cin >> a;
+//	String s(a);
+//	s[0] = '!';
+//	String ss = s;
+//	cout << a;
+//	if (a[2]=='1') {
+//		cout << "\n" << a[1];
+//	}
+//}
